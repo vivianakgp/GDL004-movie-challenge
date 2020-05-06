612bb9a8a323d6caa3e168e0e673cf5bcc633d58
26erika <develop.erika@gmail.com>
2020-05-06 08:41:05 -0500

    Random movie generator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //router
 import { changeView } from "./router/router.js";
-
+//agregoaquí
 const init = () => {
   changeView(window.location.hash);
   window.addEventListener("hashchange", () => changeView(window.location.hash));
