65c2a1c6bc6aa2f025034d61dd08d4e1ec0ee70d
viviana gomez <vivianakgp@gmail.com>
2020-05-06 21:08:03 -0500

    estilos a vista random

--- src/views/random.js
+++ src/views/random.js
@@ -17,13 +17,20 @@ if (image !== "N/A") {
 
 export default () => {
   const viewHome = `
-    <h1 class="text-center">${movie.Title}</h1>
-    <p>${movie.Runtime} | ${movie.Year} | ${movie.Rated} </p>
-    <img id='moviePoster' src=${movie.Poster}/>
-    <p>${movie.Plot}</p>
-    <p><strong>Genre: </strong>${movie.Genre}</p>
-    <p><strong>Director: </strong>${movie.Director}</p>
-    <p><strong>Stars: </strong>${movie.Actors}</p>
+  <div class="card border-danger mb-3" style="max-width: 20rem;">
+  <div class="card-header">Movie app</div>
+  <div class="card-body">
+    <h4 class="text-danger">${movie.Title}</h4>
+    <p class="card-text">${movie.Runtime} | ${movie.Year} | ${movie.Rated}</p>
+    <img src="${movie.Poster}"   id='moviePoster' class="card-img-top"/>
+    <p class="card-text"><strong>Genre: </strong>${movie.Genre}</p>
+    <p class="card-text"><strong>Director: </strong>${movie.Director}</p>
+    <p class="card-text"><strong>Stars: </strong>${movie.Actors}</p>
+    <a href="#/random" class="btn btn-danger btnWatchMovie">Watch movie</a>
+    <button type="button" class="btn btn-outline-secondary btnL">𐠪</button>
+  </div>
+
+   
     `;
 
   const btnRandom = document.createElement("a");
@@ -33,7 +40,8 @@ export default () => {
   btnHome.setAttribute("href", "#/home");
   btnHome.innerHTML = "ir a home";
   const divElemt = document.createElement("div");
-  divElemt.classList.add("position");
+  divElemt.setAttribute("class","divRandom");
+  //divElemt.classList.add("position");
 
   
   divElemt.appendChild(btnHome)
@@ -42,3 +50,21 @@ export default () => {
   ;
   return divElemt;
 };
+/**
+ * <div class="card border-danger mb-3" style="max-width: 20rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h4 class="card-title">Danger card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+ */
+/**
+ *  <h1 class="text-center">${movie.Title}</h1>
+    <p>${movie.Runtime} | ${movie.Year} | ${movie.Rated} </p>
+    <img id='moviePoster' src=${movie.Poster}/>
+    <p>${movie.Plot}</p>
+    <p><strong>Genre: </strong>${movie.Genre}</p>
+    <p><strong>Director: </strong>${movie.Director}</p>
+    <p><strong>Stars: </strong>${movie.Actors}</p>
+    
+ */
