397602dda384137bd1c151ab7915ae7b9bbf5de4
viviana gomez <vivianakgp@gmail.com>
2020-05-06 19:07:56 -0500

    se agrego diseño a home y start

--- src/homeContent.js
+++ src/homeContent.js
@@ -12,17 +12,23 @@ export default () => {
                 contentBox.innerHTML = ''
                 console.log(data);
                 if (data.Error) {
-                    contentBox.innerHTML += `<h1>Error: ${data.Error}</h1>`;
+                    contentBox.innerHTML += ` <p class="lead">Error: ${data.Error}</p>`;
                     return;
                 }
                 let content = '';
                 for (let movie of data.Search) {
                     let templateMovie = `
-                        <div style="border: 1px solid black; border-radius: 4px; width: 24%; margin: 5px; padding: 3px;">
-                        <img src="${movie.Poster}">
-                        <h1>Titulo: ${movie.Title}</h1>
-                        <h1>Año: ${movie.Year}</h1>
-                        </div>`;
+                    <div class="card border-dark mb-3" style="max-width: 20rem;">
+                        <div class="card-header">Movie App</div>
+                        <div class="card-body">
+                        <h4 class="card-title">${movie.Title}</h4>
+                        <p class="card-text">${movie.Year}</p>
+                        <img src="${movie.Poster}" class="card-img-top">
+                        <button type="button" class="btn btn-danger">wach movie</button>
+                        <button type="button" class="btn btn-outline-secondary">𐠪</button>
+                        <br/>
+                    </div>
+                        `;
                         content += templateMovie;
                 }
                 contentBox.innerHTML += content;
@@ -34,3 +40,16 @@ export default () => {
         showMovies(inputText);
     });
 };
+/*
+  for (let movie of data.Search) {
+                    let templateMovie = `
+                        <div style="border: 1px solid black; border-radius: 4px; width: 24%; margin: 5px; padding: 3px;">
+                        <img src="${movie.Poster}">
+                        <h1>Titulo: ${movie.Title}</h1>
+                        <h1>Año: ${movie.Year}</h1>
+                        </div>`;
+                        content += templateMovie;
+                }
+                <h1>Error: ${data.Error}</h1>
+*/
+
--- src/views/start.js
+++ src/views/start.js
@@ -1,25 +1,37 @@
 export default () => {
-  const viewHome = `
-    <div>
-    
-    <h1 class="text-center">CAN'T DECIDE WHAT TO WATCH?</h1>
-    
-    </div>
-    `;
 
+    //elemento h2 es la pregunta!
+  const question = document.createElement("h2");
+    question.innerHTML = "CAN'T </br>DECIDE</br> WHAT TO</br> WATCH?";
+    question.setAttribute("classs","question");
+    //boton obtener aleatoreo
   const btnRandom = document.createElement("a");
-  btnRandom.setAttribute("href", "#/random");
-  btnRandom.innerHTML = "Pick a movie for me!";
+   btnRandom.setAttribute("href", "#/random");
+   btnRandom.setAttribute("class","btn btn-danger btnRandom");
+   btnRandom.innerHTML = "Pick a movie for me!";
+    //div para el btn home
+  const divBtnHome = document.createElement("div");
+    divBtnHome.setAttribute("class","divBtnHome");
+    //btn a home
   const btnHome = document.createElement("a");
-  btnHome.setAttribute("href", "#/home");
-  btnHome.innerHTML = "&times;";
+    btnHome.setAttribute("href", "#/home");
+    btnHome.setAttribute("class","btn btn-outline-secondary btnHome");
+    btnHome.innerHTML = "X";
+    //div padre  &times;
   const divElemt = document.createElement("div");
-  divElemt.classList.add("position");
+  divElemt.setAttribute("class","divElemt");
+  //divElemt.classList.add("position");
 
-  const br = document.createElement('br')
-  divElemt.innerHTML = viewHome;
+
+  //const br = document.createElement('br')
+  //divElemt.innerHTML = viewHome;
+  //divElemt.appendChild(br)
+ 
+  divElemt.appendChild(divBtnHome);
+  divBtnHome.appendChild(btnHome);
+  divElemt.appendChild(question);
   divElemt.appendChild(btnRandom);
-  divElemt.appendChild(br)
-  divElemt.appendChild(btnHome);
+ 
+
   return divElemt;
 };
