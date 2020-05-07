f384d2c02177d91974b44b6b60e4eefe60cf9e1e
Lorena Alvarez <lorena.alvarez1294@gmail.com>
2020-05-07 02:46:46 -0500

    cambios a bootstrap, agregar columnas y filas, boton buscar y desktop

--- src/views/home.js
+++ src/views/home.js
@@ -1,43 +1,38 @@
 export default () => {
   const viewHome = `
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Movie App</a>
-  <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="navbar-collapse collapse" id="navbarColor02" style="">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#/random">Random <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#/home"></a>
-      </li>
-    </ul>
-   
-  </div>
-</nav>
-<form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" id="search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit" id="btnSearch">Search</button> 
+  <div class="row">
+    <div class="col-sm-12">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Home</a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbarColor02" style="">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#/random">Random <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/home"></a>
+          </li>
+        </ul>
+      
+      </div>
+    </nav>
+    <form class="form-inline my-2 my-lg-0">
+        <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Search" id="search">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button" id="btnSearch">Search</button>
+        </div>
+        </div>
     </form>
-    <div id="contentBox"></div>
-  
+        <div id="contentBox"></div>
+    </div>
+  </div>
   `;
   const divElemt = document.createElement('div');
   divElemt.innerHTML = viewHome;
   return divElemt;
 };
-/*
-const viewHome = `
-  <h2 class="text-center">¡Bienvenido a nuestra Home!</h2>
-  <button> <a href=#/random> Random Movie </a>  </button>
-  <input type="text" id="search">
-    <button id="btnSearch">Search</button>
-    <div id="contentBox"></div>
-  `;
-  const divElemt = document.createElement('div');
-  divElemt.innerHTML = viewHome;
-  return divElemt
-*/
+
--- src/views/random.js
+++ src/views/random.js
@@ -1,11 +1,15 @@
 export default () => {
       const viewRandom = document.createElement("div");
       viewRandom.innerHTML = `
+      <div class="row">
+      <div class="col-sm-12">
         <div id='randomContainer'>
           <div id='movieCard'></div>
           <button class="btn btn-danger btnWatchMovie" id='retry'><img class="icon" src='../img/reload.png' alt='Try again'/></button>
         </div>
-        `;
+      </div>
+      </div>
+      `;
     
       //Retry function
       viewRandom.querySelector("#retry").addEventListener("click", (e) => {
